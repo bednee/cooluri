f641ca99c44788fbf9e497c9126fd69bfb757447
maximilian-walter <github@max-walter.net>
2015-01-23 09:51:07 +0100

    Improved ext_tables.sql
    The database-structure was not correctly recognized by the
    TYPO3 Install-Tool, so it was possible to accidentally delete
    the tables "link cache" and "link_oldlinks".

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,5 +1,5 @@
 #
-# Modifying pages table
+# Table structure for table "pages"
 #
 CREATE TABLE pages (
 	tx_realurl_pathsegment varchar(30) default '',
@@ -8,8 +8,37 @@ CREATE TABLE pages (
 );
 
 #
-# Modifying pages_language_overlay table
+# Table structure for table "pages_language_overlay"
 #
 CREATE TABLE pages_language_overlay (
-	tx_realurl_pathsegment varchar(255) DEFAULT '' NOT NULL
+	tx_realurl_pathsegment varchar(255) default ''
+);
+
+#
+# Table structure for table "link_cache"
+#
+CREATE TABLE link_cache (
+	id int(10) unsigned NOT NULL auto_increment,
+	params blob,
+	url char(255) default '',
+	tstamp timestamp NOT NULL default 'CURRENT_TIMESTAMP' on update CURRENT_TIMESTAMP,
+	crdatetime datetime default '',
+	sticky tinyint(1) unsigned default '0',
+	PRIMARY KEY (id),
+	KEY url (url),
+	KEY params (params(255))
+);
+
+
+#
+# Table structure for table "link_oldlinks"
+#
+CREATE TABLE link_oldlinks (
+	id int(10) unsigned NOT NULL auto_increment,
+	link_id int(10) unsigned NOT NULL default '0',
+	url char(255) default '',
+	tstamp timestamp NOT NULL default 'CURRENT_TIMESTAMP' on update CURRENT_TIMESTAMP,
+	PRIMARY KEY (id),
+	UNIQUE id (id),
+	KEY url (url)
 );
