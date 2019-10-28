a1fcb1e77418c120d254ba4bde749247b54a523f
bednee <info@bednarik.org>
2019-10-28 09:12:27 +0100

    1.2 Release

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,13 +3,13 @@
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'CoolUri',
 	'description' => 'RealURL alternative. Have nice URLs instead of ugly with parameters. CoolUri has user-friendly XML configuration file. For simple setup, just use the one supplied with extension and you are ready to go.',
-	'version' => '1.1.5',
+	'version' => '1.2',
 	'state' => 'stable',
 	'author' => 'Jan Bednarik',
 	'author_email' => 'info@bednarik.org',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '9.0.0-9.9.99',
+			'typo3' => '8.7.0-9.9.99',
 		),
 	),
 );
